881a057bd19d17a38d1b6a2f835f0923b6781a67
OmatrixTech <144843226+OmatrixTech@users.noreply.github.com>
2023-09-30 12:35:24 +0530

    Latest Code

---
+++ WPFCustomDialogTesting/WPFCustomDialogTesting/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using WPF.Custom.PopupDialogs.Utility;
+
+namespace WPFCustomDialogTesting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ModalPopupSetup();
+        }
+
+        private void ModalPopupSetup()
+        {
+
+            ModalPopupConfigurationSetup.ModalPopupBodyBackgroundColor = new SolidColorBrush(Colors.Black);
+            ModalPopupConfigurationSetup.ModalPopupButtonBottomAlignment = HorizontalAlignment.Right;
+            ModalPopupConfigurationSetup.ModalPopupButtonWidth = 100.0;
+            ModalPopupConfigurationSetup.ModalPopupMessageTextColor = new SolidColorBrush(Colors.White);
+            ModalPopupConfigurationSetup.ModalPopupMessageTextFontSize = 15.0;
+            ModalPopupConfigurationSetup.ModalPopupButtonDisplayBackgroundColor = new SolidColorBrush(Colors.Blue);
+            ModalPopupConfigurationSetup.ModalPopupButtonTextColor = new SolidColorBrush(Colors.White);
+            ModalPopupConfigurationSetup.ModalPopupMouseHoveButtonBackgroundColor = new SolidColorBrush(Colors.OrangeRed);
+            ModalPopupConfigurationSetup.ModalpopupMouseHoverButtonForegroundColor = new SolidColorBrush(Colors.White);
+        }
+    }
+}
---
+++ WPFCustomDialogTesting/WPFCustomDialogTesting/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF.Custom.PopupDialogs.Utility;
+
+namespace WPFCustomDialogTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommonEvents.ModalConfirmDialogCallbackHandler += CommonEvents_ModalPopupCallbackHandler;
+        }
+
+        private void CommonEvents_ModalPopupCallbackHandler(object sender, ProcessEventArgs e)
+        {
+            if (e.DialogResult)
+            {
+                //Perform your task
+            }
+            else
+            {
+                //do action
+            }
+        }
+
+        private void BtnAlert_Click(object sender, RoutedEventArgs e)
+        {
+            ModalCustomAlertPopupInstanceHandler.GetInstance.ShowCustomAlertDialog(headerTitle:"Information",modalPopupMessageText:"This is a custom message box",messageBoxImageIconType:CustomMessageBoxImage.Information, modalPopupButtonText:"OK");
+            
+        }
+
+        private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            ModalCustomAlertPopupInstanceHandler.GetInstance.ShowCustomCustomConfirmDialog(headerTitle: "Information", modalPopupMessageText: "Do you want to close this popup?", messageBoxImageIconType: CustomMessageBoxImage.Information, modalPopupFirstButtonText: "OK", modalPopupSecondButtonText: "Cancel");
+        }
+    }
+}
